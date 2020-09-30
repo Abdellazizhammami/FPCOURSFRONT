3a18381ec5c2e76b89d7f90e83983ab74e10c711
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2020-09-30 20:04:48 +0200

    creation article

--- src/app/dash/gestionarticle/creer-article/creer-article.component.ts
+++ src/app/dash/gestionarticle/creer-article/creer-article.component.ts
@@ -13,49 +13,52 @@ export class CreerArticleComponent implements OnInit {
   art: any = {};
   public newArt;
   public utilisateur;
-  public fileimg= new FormData;
-  public form:FormGroup;
- 
-  public categorie="";
-  public selectedFile :File=null;
-  titre:string;
-  contenu:string;
-  
+  public fileimg = new FormData;
+  public form: FormGroup;
+  public form2: FormGroup;
+
+  public categorie = "";
+  public selectedFile: File = null;
+  titre: string;
+  contenu: string;
 
 
 
-  constructor(private rote: Router, private addArt: GesArtService, private keteb: LoginService,private fb: FormBuilder) {
+
+  constructor(private rote: Router, private addArt: GesArtService, private keteb: LoginService, private fb: FormBuilder) {
     this.form = this.fb.group({
-      
+
       chapitres: this.fb.array([])
     });
-   }
+
+    this.form2 = this.fb.group({
+      titre: String,
+      questions: this.fb.array([])
+    });
+  }
 
   ngOnInit() {
-    // this.art.titre=this.titre;
-    // this.art.contenu=this.contenu,
-    
+
     this.utilisateur = this.keteb.userrr.user;
-    console.log(this.utilisateur);
     this.art.auteur = this.utilisateur._id;
-    //this.art.chapitres= this.chapitres;
-   
-    
+
+
+
   }
 
 
   ajArt() {
-   
-    this.addArt.AjouterArts(this.selectedFile,this.art.titre,this.art.auteur,this.art.categorie).subscribe(file => {
+
+    this.addArt.AjouterArts(this.selectedFile, this.art.titre, this.art.auteur, this.art.categorie).subscribe(file => {
       this.newArt = file;
-      this.addArt.ModifArts(this.art.auteur,this.newArt.data._id,this.form.value).subscribe(file2 => {
+      this.addArt.ModifArts(this.art.auteur, this.newArt.data._id, this.form.value).subscribe(file2 => {
         this.newArt = file2;
-      } );
+      });
       this.rote.navigate(['/home']);
     })
   }
-  upImg(event){
-    this.selectedFile= event.target.files[0];
+  upImg(event) {
+    this.selectedFile = event.target.files[0];
   }
   addChap() {
     const chap = this.form.get('chapitres') as FormArray;
@@ -63,13 +66,26 @@ export class CreerArticleComponent implements OnInit {
       titre: '',
       contenu: ''
     }));
-
-    
-
   }
-  removeChap(i){
+  removeChap(i) {
     const chap = this.form.get('chapitres') as FormArray;
     chap.removeAt(i);
+
+  }
+  addQuest(){
+    console.log(this.form2.get('questions'));
+    const quest = this.form2.get('questions') as FormArray;
+    quest.push(this.fb.group({
+      enonce: '',
+      propo0:'',
+      propo1:'',
+      propo2:'',
+      propo3:'',
+      rep0:false,
+      rep1:false,
+      rep2:false,
+      rep3:false
+    }));
 
   }
 
